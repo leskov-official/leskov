f3aa261470828e761bec9aa8b81d74bfbad11121
Leskov <leskov-official@outlook.com>
2025-07-02 11:47:42 +0300

    Update slide_image.js

--- scripts/slide_image.js
+++ scripts/slide_image.js
@@ -12,23 +12,18 @@ function changeSlide(direction) {
 
   slides[newSlideIndex].classList.add('active'); 
 
-  // Сохраняем индекс текущего слайда в localStorage
   localStorage.setItem('currentSlide', newSlideIndex);
 }
 
-// Восстанавливаем индекс слайда при загрузке страницы
 window.onload = function() {
   var slides = document.querySelectorAll('.carousel .slide');
   var savedSlideIndex = localStorage.getItem('currentSlide');
 
   if (savedSlideIndex !== null) {
-    // Убираем класс 'active' с текущего слайда
     slides.forEach(slide => slide.classList.remove('active'));
 
-    // Восстанавливаем слайд, который был сохранен
     slides[savedSlideIndex].classList.add('active');
   } else {
-    // Если сохраненный слайд не найден, устанавливаем первый слайд как активный
     slides[0].classList.add('active');
   }
 };
