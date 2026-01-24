c603f800dba7e14bb5505e5e9afb0f1f7e44dc40
Leskov <leskov-official@outlook.com>
2026-01-24 14:01:35 +0200

    Update video-audio_player.js

--- scripts/video-audio_player.js
+++ scripts/video-audio_player.js
@@ -1,6 +1,6 @@
 function updateSeekBarBackground(seekBar, value, max) {
   const percent = max > 0 ? (value / max) * 100 : 0;
-  seekBar.style.background = `linear-gradient(to right, #00336a 0%, #00336a ${percent}%, #c7c7c7 ${percent}%, #c7c7c7 100%)`;
+  seekBar.style.background = `linear-gradient(to right, #ffd700 0%, #ffd700 ${percent}%, #c7c7c7 ${percent}%, #c7c7c7 100%)`;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -133,4 +133,5 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
 
